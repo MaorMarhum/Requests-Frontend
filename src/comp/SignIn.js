41104283f0caa6d1f2ff19aa72557b967b8e550a
maormarhum <maormarhum9400@gmail.com>
2023-05-11 00:03:37 +0300

    sa

--- src/comp/SignIn.js
+++ src/comp/SignIn.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import { TextField, Typography, Button } from "@mui/material";
 import axios from "axios";
 import { API_BASE_URL } from "../config";
@@ -8,7 +8,7 @@ const SignIn = ({ setName }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const history = useHistory();
+  // const history = useHistory();
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
