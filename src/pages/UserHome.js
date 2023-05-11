6096945dc46079cf84e93564c21769edb15f0587
maormarhum <maormarhum9400@gmail.com>
2023-05-11 18:06:06 +0300

    gfddd

--- src/pages/UserHome.js
+++ src/pages/UserHome.js
@@ -37,11 +37,11 @@ const UserHome = ({ name, setName, id }) => {
   };
   /* eslint-enable no-restricted-globals */
 
-  useEffect(() => {
-    if (!name) {
-      history.push("/");
-    }
-  }, [name, history]);
+  // useEffect(() => {
+  //   if (!name) {
+  //     history.push("/");
+  //   }
+  // }, [name, history]);
 
   return (
     <>
